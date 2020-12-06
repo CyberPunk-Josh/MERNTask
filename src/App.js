3c68e831e9ed928eedf38f3b6222789a23623bbb
Joshue <cristianjoshue@hotmail.com>
2020-12-06 14:12:52 -0600

    Se habilito Axios

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ import NuevaCuenta from './components/auth/NuevaCuenta';
 import Proyectos from './components/proyectos/Proyectos';
 import ProyectoState from './context/Proyectos/ProyectoState';
 import TareaState from './context/Tareas/TareasState';
+import AlertaState from './context/Alertas/alertaState';
+import AuthState from './context/Autenticacion/AuthState';
 
 
 function App() {
+  
   return (
     <ProyectoState>
       <TareaState>
-        <Router>
-          <Switch>
-            <Route exact path='/' component={Login}/>
-            <Route exact path='/nueva-cuenta' component={NuevaCuenta}/>
-            <Route exact path='/proyectos' component={Proyectos}/>
-          </Switch>
-        </Router>
+        <AlertaState>
+          <AuthState>
+            <Router>
+              <Switch>
+                <Route exact path='/' component={Login}/>
+                <Route exact path='/nueva-cuenta' component={NuevaCuenta}/>
+                <Route exact path='/proyectos' component={Proyectos}/>
+              </Switch>
+            </Router>
+          </AuthState>
+        </AlertaState>
       </TareaState>
     </ProyectoState>
   );
--- src/components/auth/NuevaCuenta.jsx
+++ src/components/auth/NuevaCuenta.jsx
@@ -1,7 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {Link} from 'react-router-dom';
+import alertaContext from '../../context/Alertas/alertaContext';
+import AuthContext from '../../context/Autenticacion/AuthContext';
 
 const NuevaCuenta = () => {
+
+    // extraer los valores del context:
+    const AlertaContext = useContext(alertaContext);
+    const {alerta, mostrarAlerta} = AlertaContext;
+
+    const authContext = useContext(AuthContext);
+    const { registrarUsuario } = authContext;
 
     // state para iniciar sesion:
     const [usuario, guardarUsuario] = useState({
@@ -26,16 +35,31 @@ const NuevaCuenta = () => {
         e.preventDefault();
 
         // validar que no haya campos vacios
-
+        if(nombre.trim() === '' || email.trim() === '' || password.trim() === ''|| confirmar.trim() === ''){
+            mostrarAlerta('Todos los campos son obligatorios', 'alerta-error');
+            return;
+        }
         // password minimo de 6 caracteres
-
+        if(password.length < 6){
+            mostrarAlerta('El password debe de ser al menos de 6 caracteres', 'alerta-error');
+            return;
+        }
         // los 2 password iguales
-
+        if(password !== confirmar){
+            mostrarAlerta('Los dos passwords no son iguales', 'alerta-error');
+            return;
+        }
         // pasarlo al action
+        registrarUsuario({
+            nombre, 
+            email,
+            password
+        })
     }
 
     return ( 
         <div className='form-usuario'>
+            { alerta ? ( <div className={`alerta ${alerta.categoria}`}> {alerta.msg} </div> )  : null }
             <div className='contenedor-form sombra-dark'>
                 <h1>Obtener una Cuenta</h1>
 
